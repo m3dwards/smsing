fc580ed93a883bdc8816c84cc12d8f39acf6cdef
Max Edwards <maxedwar@thoughtworks.com>
2016-01-02 16:10:48 +0000

    added dummy message

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ def hello_world():
 
 @app.route('/message')
 def message():
-    return 'Message'
+    #?msisdn=19150000001&to=12108054321
+    #&messageId=000000FFFB0356D1&text=This+is+an+inbound+message
+    #&type=text&message-timestamp=2012-08-19+20%3A38%3A23
+    text = request.args.get('text')
+    number = request.args.get('msisdn')
+    
+    if (text.lower() == "max"):
+        return "Congratulations"
+    
+    return text + number
 
 @app.route('/voice')
 def voice():
