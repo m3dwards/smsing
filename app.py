b5cad97927ed97b3b1ce2b3d341462b8254db16d
Max Edwards <maxedwar@thoughtworks.com>
2016-01-02 16:36:54 +0000

    first stab at message

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
+
+apikey = os.environ('apikey')
+apisecret = os.environ('apisecret')
+phone = os.environ('phone')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 def send_txt(message, to):
-    pass
+    requests.get('https://rest.nexmo.com/sms/json?api_key=' + apikey + '&api_secret=' + apisecret + '&from=' + phone  + '&to=' + to + '&text=' + message)
     #https://rest.nexmo.com/sms/json?api_key=xxxxxxxx&api_secret=xxxxxxxx&to=xxxxxxxxxxxx&from=NexmoWorks&text=hello+from+Nexmo
 
 @app.route('/message')
@@ -22,7 +27,9 @@ def message():
         number = request.args.get('msisdn')
     
         if (text.lower() == "max"):
-            x = 1
+            send_txt("00000001 00011010", number)
+        else:
+            send_txt("You got it wrong. " + text + " did not receive the Ray Mears book. Try again Becki.", number)
 
     except:
         pass
